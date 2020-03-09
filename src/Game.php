b25655c58d51061d113d59caae78820b7d3251ac
sione-jack-ohara <jack.ohara@sione.co.uk>
2020-03-09 17:27:53 +0000

    fix: oneSpare (green)

--- src/Game.php
+++ src/Game.php
@@ -7,13 +7,30 @@ final class Game
      */
     private $score = 0;
 
+    /**
+     * @var array
+     */
+    private $rolls = [];
+
     public function roll(int $pins): void
     {
-        $this->score += $pins;
+        $this->rolls[count($this->rolls)] = $pins;
     }
 
     public function getScore(): int
     {
-        return $this->score;
+        $score = 0;
+        $rollIndex = 0;
+
+        for ($frame = 0; $frame < 10; $frame++, $rollIndex += 2) {
+            $frameScore = $this->rolls[$rollIndex] + $this->rolls[$rollIndex + 1];
+            if ($frameScore === 10) {
+                $frameScore += $this->rolls[$rollIndex + 2];
+            }
+
+            $score += $frameScore;
+        }
+
+        return $score;
     }
 }
