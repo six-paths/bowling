b7468622b2e402518daf14539d3f6c2fb2a1c110
sione-jack-ohara <jack.ohara@sione.co.uk>
2020-03-09 17:12:52 +0000

    refactor: extract rollMany method (blue)

--- tests/BowlingGameTest.php
+++ tests/BowlingGameTest.php
@@ -16,19 +16,22 @@ final class BowlingGameTest extends TestCase
 
     public function testGutterGame(): void
     {
-        for ($i = 0; $i < 20; $i++) {
-            $this->game->roll(0);
-        }
+        $this->rollMany(20, 0);
 
         $this->assertEquals(0, $this->game->getScore());
     }
 
     public function testAllOnesGame(): void
     {
-        for ($i = 0; $i < 20; $i++) {
-            $this->game->roll(1);
-        }
+        $this->rollMany(20, 1);
 
         $this->assertEquals(20, $this->game->getScore());
+    }
+
+    private function rollMany(int $rolls, int $pins): void
+    {
+        for ($i = 0; $i < $rolls; $i++) {
+            $this->game->roll($pins);
+        }
     }
 }
